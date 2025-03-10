bf56f850c8bafb0194289a1ec8c1069c8ad1e2f1
tuan.tai <tttuan2703@gmail.com>
2025-03-11 00:58:16 +0700

    Fix GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,9 @@
 import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
 
-// If you are not going to use vercel analytics you can delete the analytics
-// If you want to remove vercel from your project you can do npm uninstall vercel
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  output: "static",
+  base: "/tttuan2703.github.io/",
+  build: {
+    outDir: "docs"
+  }
 });
-
-//If you don't have vercel the configuration will be empty
-
-//ej : export default defineConfig({});
