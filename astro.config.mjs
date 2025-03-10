e20462f47fd4d5859e6a34c13facd4e480244804
tuan.tai <tttuan2703@gmail.com>
2025-03-11 01:10:13 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: "static",
-  base: "/tttuan2703.github.io/",
-  build: {
-    outDir: "docs"
-  }
+  outDir: 'docs', // Change output folder to docs
 });
