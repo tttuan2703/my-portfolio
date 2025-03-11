1f5424700faf857e8d1449502b873cd12731b7df
Tuan Tai <mailto:tuan.tai@s3corp.com.vn>
2025-03-11 10:51:22 +0700

    clean code

--- astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://tttuan2703.github.io',  // Set your site's domain or GitHub Pages URL
-  output: 'static'  // Ensure it's set to 'static' for full compatibility
-});
